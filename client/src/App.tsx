85eddb73b201a27f89947c9422b6b898ff63758d
Jack Allen <jackallensemail+git@gmail.com>
2020-05-01 00:07:07 +0100

    Face.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,7 +9,12 @@ function App() {
 
   return (
     <div className="App">
-      Steve.
+
+      <img src="https://media-exp1.licdn.com/dms/image/C5603AQGjGcewBcHnxg/profile-displayphoto-shrink_800_800/0?e=1593648000&v=beta&t=SE_i_ql3K5cUGO9Ye2X_p6Nm4stViEoazrVBcXNGWsU"/>
+
+      36 year old baby man looking for love. Preferably a hot lady, but at this point i'll take anything with a pulse. Note: pulse optional.
+
+
     </div>
   );
 }
